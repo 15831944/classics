e9bd096028e8383929a7b2d19bfd6f6ac1581acb
Brett V. Forsgren <brett.forsgren@outlook.com>
2016-01-17 13:39:18 -0800

    create proper msbuild tasks and download Whittaker's Words

---
+++ src/IxMilia.Classics/Tasks/DownloadFile.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) IxMilia.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.IO;
+using System.Net;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace IxMilia.Classics2.Tasks
+{
+    public class DownloadFile : Task
+    {
+        [Required]
+        public string Url { get; set; }
+
+        [Required]
+        public string Destination { get; set; }
+
+        public override bool Execute()
+        {
+            if (!File.Exists(Destination))
+            {
+                new WebClient().DownloadFile(Url, Destination);
+            }
+
+            return true;
+        }
+    }
+}
