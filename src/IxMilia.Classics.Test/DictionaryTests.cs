f5cd1bfd536a1ff67e7e46e8f270a4ddcfa08c28
Brett V. Forsgren <brett.forsgren@outlook.com>
2016-01-25 10:47:53 -0800

    add test project

---
+++ src/IxMilia.Classics.Test/DictionaryTests.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) IxMilia.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Linq;
+using Xunit;
+
+namespace IxMilia.Classics.Test
+{
+    public class DictionaryTests
+    {
+        static readonly LatinDictionary Latin = new LatinDictionary();
+
+        private DictionaryEntry GetSingleEntry(string word)
+        {
+            return Latin.GetDefinitions(word).Single();
+        }
+
+        [Fact]
+        public void NounPartOfSpeechParseTest()
+        {
+            var word = GetSingleEntry("porta");
+            Assert.Equal(Declension.First, word.Declension);
+            Assert.Equal(Gender.Feminine, word.Gender);
+        }
+    }
+}
