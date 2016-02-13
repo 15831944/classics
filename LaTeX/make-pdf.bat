b6b494cfd23aaa430c8fbd92f6aec543b8222c68
Brett V. Forsgren <brett.forsgren@outlook.com>
2016-02-12 17:37:39 -0800

    order per-page glossary entries
    also italicize per-page items (italics will be inverted eventually)

--- LaTeX/make-pdf.bat
+++ LaTeX/make-pdf.bat
@@ -1,3 +1,7 @@
+del Aeneid.pdf
+del Aeneid.ppo
+del Aeneid.ppg
+
 pdflatex Aeneid.tex
 if errorlevel 1 goto error
 
@@ -8,6 +12,9 @@ makeindex -s Aeneid.ist -o Aeneid.common-gls Aeneid.common-glo
 if errorlevel 1 goto error
 
 makeindex -s Aeneid.ist -o Aeneid.uncommon-gls Aeneid.uncommon-glo
+if errorlevel 1 goto error
+
+..\src\Binaries\Debug\IxMilia.Classics.ReorderFootnotes.exe Aeneid.ppg Aeneid.ppo
 if errorlevel 1 goto error
 
 pdflatex Aeneid.tex
---
+++ src/IxMilia.Classics.ReorderFootnotes/Program.cs
@@ -0,0 +1,55 @@
+﻿// Copyright (c) IxMilia.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace IxMilia.Classics.ReorderFootnotes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Expected two arguments: glossary page order and glossary word order");
+                Environment.Exit(1);
+            }
+
+            var pageOrderFile = args[0];
+            var wordOrderFile = args[1];
+
+            var wordKeys = File.ReadAllLines(wordOrderFile);
+
+            int currentPage = -1;
+            int wordsPerPage = 0;
+            int keyOffset = 0;
+            var sortedWords = new List<string>();
+            foreach (var line in File.ReadAllLines(pageOrderFile))
+            {
+                if (line.StartsWith("PAGE="))
+                {
+                    Console.WriteLine($"Writing {wordsPerPage} for page {currentPage}");
+                    var wordsOnThisPage = wordKeys.Skip(keyOffset)
+                        .Take(wordsPerPage)
+                        .OrderBy(w => w);
+                    sortedWords.AddRange(wordsOnThisPage);
+                    currentPage = int.Parse(line.Split('=')[1]);
+                    keyOffset += wordsPerPage;
+                    wordsPerPage = 0;
+                }
+                else if (line == "WORD")
+                {
+                    wordsPerPage++;
+                }
+                else
+                {
+                    throw new InvalidOperationException("Unexpected line value: " + line);
+                }
+            }
+
+            File.WriteAllLines(wordOrderFile, sortedWords);
+        }
+    }
+}
